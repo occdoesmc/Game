c247cf6e2066d75bf4bfae17d259e672a320e0e1
Orlando Castiglioni <occ.github@hairesis.com>
2018-03-29 18:57:16 +0200

    Implement view layer

--- Game/Controller/ViewController.swift
+++ Game/Controller/ViewController.swift
@@ -1,3 +1,16 @@
 import UIKit
 
-final class ViewController: UIViewController {}
+final class ViewController: UIViewController {
+    
+    @IBOutlet private var columnButtons: [UIButton] = []
+    @IBOutlet private var discViews: [CircleView] = []
+
+    @IBAction private func userDidTapColumnButton(_ columnButton: UIButton) {
+        print(columnButtons.index(of: columnButton)!)
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+}
---
+++ Game/View/CircleView.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+@IBDesignable final class CircleView: UIView {
+
+    @IBInspectable var isInverted: Bool = false {
+        didSet {
+            updateMask()
+        }
+    }
+
+    @IBInspectable var circleMargin: CGFloat = 0 {
+        didSet {
+            updateMask()
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        updateMask()
+    }
+
+    private func updateMask() {
+        let imageRenderer = UIGraphicsImageRenderer(bounds: bounds)
+        let image = imageRenderer.image { context in
+            let underlyingContext = context.cgContext
+            let circleDiameter = min(bounds.size.width, bounds.size.height)
+            let circleRadius = circleDiameter / 2
+            let circleRadiusWithMargin = circleRadius - circleMargin
+            let circleCenter = CGPoint(x: bounds.midX, y: bounds.midY)
+            underlyingContext.beginPath()
+            underlyingContext.addArc(center: circleCenter, radius: circleRadiusWithMargin, startAngle: 0, endAngle: .pi * 2, clockwise: true)
+            if isInverted {
+                let bottomRightCorner = CGPoint(x: bounds.maxX, y: bounds.maxY)
+                let bottomLeftCorner = CGPoint(x: 0, y: bounds.maxY)
+                let topLeftCorner = bounds.origin
+                let topRightCorner = CGPoint(x: bounds.maxX, y: 0)
+                underlyingContext.addLines(between: [bottomRightCorner, bottomLeftCorner, topLeftCorner, topRightCorner])
+            }
+            underlyingContext.fillPath()
+        }
+        let imageView = UIImageView(image: image)
+        imageView.frame = bounds
+        mask = imageView
+    }
+
+}
