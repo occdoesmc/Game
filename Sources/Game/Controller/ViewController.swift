6ed244000b12473afa970b35a3fffbde14f53269
Orlando Castiglioni <occ.github@hairesis.com>
2018-06-08 19:17:46 +0100

    Create empty board when game starts

--- Sources/Game/Controller/ViewController.swift
+++ Sources/Game/Controller/ViewController.swift
@@ -5,12 +5,44 @@ final class ViewController: UIViewController {
     @IBOutlet private var boardPositionButtons: [UIButton] = []
     @IBOutlet private var boardDiscViews: [CircleView] = []
 
-    @IBAction private func userDidTapOn(button: UIButton) {
-        print("\(boardPositionButtons.index(of: button))")
+    private var board: Board!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         board = Board(delegate: self)
     }
 
+    @IBAction private func userDidTapOn(button: UIButton) {
+        let buttonIndex = boardPositionButtons.index(of: button)!
+        board.placeDisc(atIndex: buttonIndex)
+    }
+    
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    
+}
+
+extension ViewController: BoardDelegate {
+
+    func board(_ board: Board, didUpdate disc: BoardDisc, atIndex index: Int) {
+        let discColorName: String
+        let discAccessibilityLabel: String
+        
+        switch disc {
+        case .none:
+            discColorName = "Colors/Gray"
+            discAccessibilityLabel = NSLocalizedString("board.disc.empty", comment: "")
+        case .player1:
+            discColorName = "Colors/Red"
+            discAccessibilityLabel = NSLocalizedString("board.disc.player1", comment: "")
+        case .player2:
+            discColorName = "Colors/Yellow"
+            discAccessibilityLabel = NSLocalizedString("board.disc.player2", comment: "")
+        }
+        
+        boardDiscViews[index].backgroundColor = UIColor(named: discColorName)
+        boardPositionButtons[index].accessibilityLabel = discAccessibilityLabel
     }
     
 }
--- Sources/Game/Model/Board.swift
+++ Sources/Game/Model/Board.swift
@@ -2,30 +2,34 @@ import Foundation
 
 enum BoardDisc {
 
+    case none
     case player1
     case player2
-    case none
 
 }
 
 protocol BoardDelegate: AnyObject {
 
-    func board(_ board: Board, didPlaceDiscAtIndex index: Int)
-    func board(_ board: Board, didUpdate: BoardDisc, atIndex index: Int)
-    func board(_ board: Board, didUpdateActive isActive: Bool, forDiscAtIndex index: Int)
-    func board(_ board: Board, didEndGameWithMessage message: String)
+//    func board(_ board: Board, didPlaceDiscAtIndex index: Int)
+    func board(_ board: Board, didUpdate disc: BoardDisc, atIndex index: Int)
+//    func board(_ board: Board, didUpdateActive isActive: Bool, forDiscAtIndex index: Int)
+//    func board(_ board: Board, didEndGameWithMessage message: String)
 
 }
 
 final class Board {
 
     weak var delegate: BoardDelegate?
-
-    init() {
+    
+    init(delegate: BoardDelegate) {
+        self.delegate = delegate
+        for discIndex in 0..<42 {
+            delegate.board(self, didUpdate: .none, atIndex: discIndex)
+        }
     }
 
-    func placeDisc(inColumn column: Int) {
-
+    func placeDisc(atIndex index: Int) {
+        
     }
 
 }
